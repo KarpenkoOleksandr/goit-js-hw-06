3be5b28900196b823bbceb427f181c403b772eb7
KarpenkoOleksandr <131707071+KarpenkoOleksandr@users.noreply.github.com>
2023-08-21 22:42:22 +0300

    Update task-04.js

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,9 +1,19 @@
-const buttonMinus = document.querySelector('#decrement');
+const valueEl = document.getElementById('value');
+
+const buttonMinus = document.querySelector('[data-action="decrement"]');
 buttonMinus.addEventListener("click", onClickDecrement);
-const buttonPlus = document.querySelector('#increment');
+
+const buttonPlus = document.querySelector('[data-action="increment"]');
 buttonPlus.addEventListener("click", onClickIncrement);
-const counterValue = document.querySelector('#value');
-counterValue = 0;
+
+let counterValue = 0;
+
 function onClickDecrement(evt) {
-    console.log('Its a click!');
+    counterValue -= 1;
+    valueEl.textContent = counterValue;
+}
+
+function onClickIncrement(evt) {
+    counterValue += 1;
+    valueEl.textContent = counterValue;
 }
