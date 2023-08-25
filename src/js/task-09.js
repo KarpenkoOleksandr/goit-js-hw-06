1547fcf78e8db481c455138a0084dad3372de0b9
KarpenkoOleksandr <131707071+KarpenkoOleksandr@users.noreply.github.com>
2023-08-25 21:42:44 +0300

    Tasks 9-10

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const spanColor = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+changeColorButton.addEventListener('click', onChangeBgColor);
+function onChangeBgColor() {
+  spanColor.textContent = getRandomHexColor();
+  body.style.backgroundColor = spanColor.textContent;
+};
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,43 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputNum = document.querySelector('[type="number"]');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const divBoxes = document.querySelector('#boxes');
+
+divBoxes.style.display = 'flex';
+divBoxes.style.flexDirection = 'column';
+divBoxes.style.alignItems = 'center';
+divBoxes.style.padding = '20px';
+
+createBtn.addEventListener('click', () => {
+  if (
+    Number(inputNum.value.trim()) > Number(inputNum.max) ||
+    Number(inputNum.value.trim()) < Number(inputNum.min)
+  ) {
+    alert('Please enter number from 1 to 100');
+  } else {
+    createBoxes(inputNum.value.trim());
+  }
+  inputNum.value = '';
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  inputNum.value = '';
+  divBoxes.innerHTML = '';
+}
+
+function createBoxes(amount) {
+  let size = 30;
+  const boxesArray = [];
+  for (let i = 0; i < amount; i += 1) {
+    size += 10 * i;
+    const div = `<div class="item" style="display: block; margin-bottom: 10px; background-color: ${getRandomHexColor()}; width: ${size}px; height: ${size}px;"></div>`;
+    boxesArray.push(div);
+  }
+  divBoxes.insertAdjacentHTML('beforeend', boxesArray.join(''));
+}
